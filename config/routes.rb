8c7916644abad3c0d6cb6900c1c90697b3dc2b1f
Ziad <ziadgholmish@gmail.com>
2017-02-26 11:46:32 +0300

    add the home root to the blog

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :articles
+
+  #the root route to my app to redirect the user to the home page of the blog
+  root 'articles#index'
+
 end
