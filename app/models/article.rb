592c5f037957d2dab70408ee77a5950b8667d9fc
Ziad Gholmish <ZiadGholmish@gmail.com>
2017-04-21 11:42:16 +0300

    add many to many asscoiation for articles and categories

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,7 @@
 class Article < ActiveRecord::Base
 
+  has_many :article_categories
+  has_many :categories, through: :article_categories
   belongs_to :user
 
   validates_presence_of :title, :desc
